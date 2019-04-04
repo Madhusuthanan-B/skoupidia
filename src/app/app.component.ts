d1fa5a0aee34e0e41a1d3ee4839ed11b9f66fbfa
MadhuB <madhub081011@gmail.com>
2019-04-04 22:55:48 +0530

    [PWA] pwa setup with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'main';
+export class AppComponent implements OnInit {
+
+  constructor(public swUpdate: SwUpdate) {
+  }
+
+  ngOnInit() {
+    this.reloadCache();
+  }
+
+  reloadCache() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New Version Available! Would you like to update?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
